3f0d176794f1b06ef8612e73b9287eb925acbd55
Sun Junwen <sunjw8888@gmail.com>
2018-11-19 15:26:36 +0800

    Code cleanup.

--- main.js
+++ main.js
@@ -31,8 +31,6 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   app.quit()
 })
 
