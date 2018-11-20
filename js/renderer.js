88bd32cd3f3007b2298ef862b6d2d83ad0a377ed
Sun Junwen <sunjw8888@gmail.com>
2018-11-20 18:47:55 +0800

    Implement displayDevices.

--- js/renderer.js
+++ js/renderer.js
@@ -1,17 +1,53 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
 const Utils = require('./utils.js');
 const ADBHelper = require('./adb-helper.js');
 
 var divFileList = 0;
 
-$(function () {
+function init() {
     divFileList = $('#divFileList');
-    divFileList.empty();
+}
 
+function clearFileList() {
+    divFileList.empty();
+}
+
+function displayDevices() {
     var adbHelper = new ADBHelper.ADBHelper('adb');
     adbHelper.getDevices((adbDevices) => {
-        divFileList.text('Found devices: ' + adbDevices.length);
+        clearFileList();
+        if (adbDevices.length == 0) {
+            divFileList.text('No device found');
+        } else {
+            for (const device of adbDevices) {
+                var deviceAvailable = (device.status == 'device');
+
+                var divDeviceId = $('<div/>').addClass('deviceId')
+                    var divDeviceLine = $('<div/>').addClass('deviceLine');
+                if (deviceAvailable) {
+                    var aDeviceLink = $('<a/>').text(device.id).attr('href', '#' + device.id).click(function () {
+                            const deviceId = $(this).attr('href').substr(1);
+                            alert('Device [' + deviceId + '] selected');
+                            Utils.log('Device [' + deviceId + '] selected');
+                            return false;
+                        });
+                    divDeviceId.append(aDeviceLink);
+                } else {
+                    divDeviceId.text(device.id);
+                }
+                divDeviceLine.append(divDeviceId);
+
+                var divDeviceStatus = $('<div/>').addClass('deviceStatus').text(device.status);
+                divDeviceLine.append(divDeviceStatus);
+
+                divFileList.append(divDeviceLine);
+            }
+        }
     });
+}
+
+$(function () {
+    init();
+    clearFileList();
+
+    displayDevices();
 });
