06e62a29d962472a8930cb347d011efbd8b5ab4d
Sun Junwen <sunjw8888@gmail.com>
2020-06-24 17:22:42 +0800

    Code format.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, dialog, ipcMain} = require('electron')
+const { app, BrowserWindow, dialog, ipcMain } = require('electron')
 
 const electronLocalshortcut = require('electron-localshortcut')
 const windowStateKeeper = require('electron-window-state')
@@ -14,7 +14,7 @@ const utils = require('./js/utils.js')
 let mainWindow
 let transferCount = 0
 
-function createWindow () {
+function createWindow() {
   // Load the previous state with fallback to defaults
   let mainWindowState = windowStateKeeper({
     defaultWidth: 900,
@@ -56,12 +56,11 @@ function createWindow () {
     if (transferCount > 0) {
       dialog.showMessageBox(
         mainWindow, {
-          type: 'info',
-          buttons: ['OK'],
-          title: 'Cannot exit',
-          message: 'Still transferring, cannot exit!'
-        }
-      )
+        type: 'info',
+        buttons: ['OK'],
+        title: 'Cannot exit',
+        message: 'Still transferring, cannot exit!'
+      })
       e.preventDefault()
     }
   })
@@ -78,7 +77,7 @@ function createWindow () {
   mainWindow.webContents.on('did-finish-load', () => {
     // Set user's downloads directory path to renderer.
     mainWindow.webContents.send('set-downloads-path', app.getPath('downloads'))
- })
+  })
 
   var accelerator = 'Ctrl+F'
   if (utils.isMacOS()) {
@@ -88,7 +87,7 @@ function createWindow () {
     mainWindow.webContents.send('on-find')
   })
 
-   // Let us register listeners on the window, so we can update the state
+  // Let us register listeners on the window, so we can update the state
   // automatically (the listeners will be removed when the window is closed)
   // and restore the maximized or full screen state
   mainWindowState.manage(mainWindow)
