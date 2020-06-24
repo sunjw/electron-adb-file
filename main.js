6fcc53a8fd0b518b6d0e159a4b2db143b0cfa03f
Sun Junwen <sunjw8888@gmail.com>
2020-06-24 17:31:25 +0800

    Tweak frameless window on macOS.

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function createWindow() {
   // Create the browser window.
   var titleBarStyleOS = 'default'
   if (utils.isMacOS()) {
-    titleBarStyleOS = 'hidden'
+    titleBarStyleOS = 'hiddenInset'
   }
   mainWindow = new BrowserWindow({
     x: mainWindowState.x,
