cd3936fb3080ca9da91823754eb8f9524292f01b
Sun Junwen <sunjw8888@gmail.com>
2021-08-27 10:54:39 +0800

    Refactor on listfilter.

--- js/listfilter.js
+++ js/listfilter.js
@@ -1,13 +1,11 @@
 const Utils = require('./utils');
 
-const nextIconSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/></svg>';
-
 class ListFilter {
 
     constructor(webContent) {
         this.webContent = webContent;
 
-        this.findBoxId = 'filterDivFindBox';
+        this.filterBoxId = 'filterDivFindBox';
         this.initiated = false;
         this.findBoxShown = false;
         this.matchCase = false;
@@ -23,8 +21,8 @@ class ListFilter {
         this.aClose = null;
     }
 
-    setFindBoxId(findBoxId) {
-        this.findBoxId = findBoxId;
+    setFindBoxId(filterBoxId) {
+        this.filterBoxId = filterBoxId;
     }
 
     setContainerElem(containerElem) {
@@ -39,7 +37,7 @@ class ListFilter {
         }
         this.initiated = true;
 
-        this.divFindBox = $('<div/>').attr('id', this.findBoxId).addClass('filterFindBox');
+        this.divFindBox = $('<div/>').attr('id', this.filterBoxId).addClass('filterFindBox');
 
         this.inputToFind = $('<input/>').attr('type', 'text').addClass('filterInputFind');
         this.inputToFind[0].onkeydown = function (e) {
@@ -80,6 +78,7 @@ class ListFilter {
         this.divFindBox.addClass('filterShow');
         this.focusInput();
         this.findBoxShown = true;
+        Utils.log('openFindBox');
     }
 
     closeFindBox() {
@@ -89,6 +88,7 @@ class ListFilter {
         this.divFindBox.removeClass('filterShow');
         this.stopFind();
         this.findBoxShown = false;
+        Utils.log('closeFindBox');
     }
 
     focusInput() {
